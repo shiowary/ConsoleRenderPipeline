81dbaa0f50c32397311b1e6ca944b4716504dbf0
Shio <L.setzen@gmx.de>
2025-09-25 21:31:45 +0200

    Projektdateien hinzufügen.

---
+++ RenderingPipeline/Program.cs
@@ -0,0 +1,326 @@
+﻿/*
+ 
+ 
+ 
+*/
+
+
+using System;
+using System.Drawing;
+using System.Numerics;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace RenderPipeline
+{
+
+    public class RenderClass
+    {
+        public Screen screen = new Screen();
+        private bool running = true;
+        public static void Main(string[] args)
+        {
+            //sRenderClass main = new RenderClass();
+            //main.DrawScreen();
+            RenderClass main = new RenderClass();
+            main.DrawScreen(100);
+            Console.ReadKey();
+
+
+        }
+        private async Task DrawScreen(int cycleDelay)
+        {
+            int i = 0;
+            while (running)
+            {
+                screen.ScreenHandler(cycleDelay,i);
+                if (Console.KeyAvailable)
+                {
+                    var key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.Escape)
+                        running = false;
+                }
+
+                await Task.Delay(cycleDelay);
+                i++;
+            }
+        }
+    }
+
+    public class Pixel
+    {
+        public Pixel(Vector3 pos, char px = '+', ConsoleColor col = ConsoleColor.White) {
+            this.pos = pos;
+            this.px = px;
+            this.color = col;
+        }
+        public Vector3 pos = new Vector3();
+        public char px;
+        public ConsoleColor color;
+    }
+
+    public class Screen
+    {
+        private List<Pixel> pixels = new List<Pixel>();
+        private List<Pixel> nextPixels = new List<Pixel>();
+        private List<(int,int)> buffer = new List<(int,int)> (); //list of coordinates which will become the pixels of the next step
+
+        private HelperFunc h = new HelperFunc();
+        public void renderScreen() {
+            foreach (Pixel pixel in pixels)
+            {
+                (int,int) px = h.TranslateToConsoleScreen((int)pixel.pos.x, (int)pixel.pos.y);
+                h.ReplaceLineinColor(px.Item1,px.Item2, pixel.px.ToString(),ConsoleColor.Black, pixel.color);
+            }
+        }
+
+        public void createLineInBuffer(int x0,int y0,int x1, int y1)
+        {
+            Vector3 origin = new Vector3(x0, y0);
+            Vector3 line = new Vector3(x1, y1);
+            pixels.AddRange(h.convertToPixels(h.DrawLine(origin,line)));
+        }
+
+
+        public void ScreenHandler(int refreshdelay, int iteration)
+        {
+            //clear current internal pixels
+            pixels.Clear();
+            pixels = nextPixels;
+
+            // we currently fill the buffer "manually"
+            buffer.AddRange(DrawInBuffer(iteration));
+            //
+
+            //fill the next pixel itteration for next frame
+            nextPixels = h.convertToPixels(buffer);
+            //clear window fully
+            Console.Clear();
+            //redraw all pixels within the pixel buffer
+            renderScreen();
+            //clear the buffer
+            buffer.Clear();
+        }
+
+        public List<(int,int)> DrawInBuffer(int iteration, float other = 0f, string args = "")
+        {
+            List<(int,int)> buffer = new List<(int,int)> ();
+
+            //do some magic to create buffer logic within this
+
+            //for now we do clock
+            buffer.AddRange(h.daclock(iteration * 5));
+
+            return buffer;
+        }
+
+        
+    }
+
+
+    public class HelperFunc
+    {
+        public void ReplaceLine(int left, int top, string txt) {
+            Console.SetCursorPosition(left, top);
+            Console.Write(txt);
+        }
+        public Pixel convertToPixel((int, int) pixel, char px = '+', ConsoleColor col = ConsoleColor.White)
+        {
+            Vector3 tempVector = new Vector3(pixel.Item1, pixel.Item2);
+            Pixel temp = new Pixel(tempVector, px, col); //generate a default pixel for now
+            return temp;
+        }
+
+        public List<Pixel> convertToPixels(List<(int,int)> px2, List<(int,int,int)> px3 = null)
+        {
+            List<Pixel> newpixels = new List<Pixel>();
+            foreach ((int,int) pixelcoord in px2)
+            {
+                Pixel temp = convertToPixel(pixelcoord);
+                newpixels.Add(temp);
+            }
+            return newpixels;
+        }
+
+        //Bresenham’s algorithm to draw a line from (0,0) to the vector3 point
+        //this part is stolen from stack overflow
+        public List<(int, int)> DrawLine(Vector3 origin, Vector3 line)
+        {
+            List<(int, int)> points = new List<(int, int)>();
+            int x0 = (int)origin.x;
+            int y0 = (int)origin.y;
+            int x1 = (int)line.x;
+            int y1 = (int)line.y;
+            int dx = Math.Abs(x1 - x0);
+            int dy = Math.Abs(y1 - y0);
+            int sx = x0 < x1 ? 1 : -1;
+            int sy = y0 < y1 ? 1 : -1;
+            int err = dx - dy;
+            while (true)
+            {
+                points.Add((x0, y0));
+                if (x0 == x1 && y0 == y1) break;
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+            return points;
+        }
+
+        public void ReplaceLineinColor(int left, int top,string txt, ConsoleColor backcol = ConsoleColor.Black, ConsoleColor forecol = ConsoleColor.White)
+        {
+            ConsoleColor baseforcol = ConsoleColor.White;
+            ConsoleColor basebackcol = ConsoleColor.Black;
+            Console.ForegroundColor = forecol;
+            Console.BackgroundColor = backcol;
+            ReplaceLine(left, top, txt);
+            Console.ForegroundColor=baseforcol;
+            Console.BackgroundColor=basebackcol;
+        }
+
+        public int calculatebuffer()
+        {
+            return 0;
+        }
+
+        public (int,int) FindAcualCentre() //its the center of the current console window, this is an absolute number
+        {
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+            int centerX = width / 2;
+            int centerY = height / 2;
+            return (centerX, centerY);
+        }
+
+        //Translates the coordinate given into a coordinate that the console understands aka (index from top, index from left)
+        public (int, int) TranslateToConsoleScreen(int x, int y, int z = 0)
+        {
+            var (centerX, centerY) = FindAcualCentre();
+            // X grows right, console X grows right
+            int screenX = centerX + x;
+            // Y grows up but console Y grows down
+            int screenY = centerY - y;
+            // Clamp to console bounds
+            screenX = Math.Clamp(screenX, 0, Console.WindowWidth - 1);
+            screenY = Math.Clamp(screenY, 0, Console.WindowHeight - 1);
+            return (screenX, screenY);
+        }
+
+
+
+        public List<(int,int)> daclock(int iteration)
+        {
+            float angle = -(iteration % 360) * (float)(Math.PI / 180.0);
+            int length = 10;
+            int endX = (int)(length * Math.Cos(angle));
+            int endY = (int)(length * Math.Sin(angle));
+            return DrawLine(new Vector3(0, 0), new Vector3(endX, endY));
+        }
+
+
+
+        public void writeLineSlow(string Line, int delay)
+        {
+            foreach (var item in Line)
+            {
+                Console.Write(item);
+                Thread.Sleep(delay);
+            }
+            Console.Write("\n");
+        }
+
+
+        public class ScreenMath
+        {
+            private HelperFunc helper = new HelperFunc();
+            private List<(int,int)> accumolator = new List<(int,int)> ();
+
+
+
+            //modified code and ideas from https://stackoverflow.com/questions/11075505/get-all-points-within-a-triangle
+            public List<(int,int)>GetTriangle(Vector3 a, Vector3 b, Vector3 c)
+            {
+                List<(int,int)> tempbuffer = new List<(int,int)> ();
+                tempbuffer.AddRange(helper.DrawLine(a, b));
+                tempbuffer.AddRange(helper.DrawLine(a, c));
+                tempbuffer.AddRange(helper.DrawLine(b, c));
+                return tempbuffer;
+            }
+
+            public List<(int,int)> FillTriangle(Vector3 a, Vector3 b, Vector3 c)
+            {
+                var v1 = (x: (int)a.x, y: (int)a.y);
+                var v2 = (x: (int)b.x, y: (int)b.y);
+                var v3 = (x: (int)c.x, y: (int)c.y);
+                List<(int,int)>points = new List<(int,int)> ();
+                //sort the tiangle points by size to magic
+
+                var verts = new List<(int x, int y)> { v1, v2, v3 }
+                .OrderBy(v => v.y).ToList();
+                v1 = verts[0]; v2 = verts[1]; v3 = verts[2];
+                //having them in order allows us to make assumptions
+
+                for (int y = v1.y; y <= v3.y; y++)
+                {
+                    bool secondHalf = y > v2.y || v2.y == v1.y;
+                    int segmentY = secondHalf ? v3.y - v2.y : v2.y - v1.y;
+                    int segmentOffset = secondHalf ? y - v2.y : y - v1.y;
+
+                    float alpha = (float)(y - v1.y) / (v3.y - v1.y);
+                    float beta = (float)segmentOffset / segmentY;
+
+                    int ax = v1.x + (int)((v3.x - v1.x) * alpha);
+                    int bx = secondHalf
+                        ? v2.x + (int)((v3.x - v2.x) * beta)
+                        : v1.x + (int)((v2.x - v1.x) * beta);
+
+                    if (ax > bx) (ax, bx) = (bx, ax);
+
+                    for (int x = ax; x <= bx; x++)
+                        points.Add((x, y));
+                }
+
+                return points;
+
+            }
+
+            private int InterpX((int x, int y) a, (int x, int y) b, int y)
+            {
+                if (a.y == b.y) return a.x;
+                return a.x + (b.x - a.x) * (y - a.y) / (b.y - a.y);
+            }
+
+            //Modified to fit my needs with T:Vector3 
+            public  Vector3 GetTriangleCenter<T>(T p0, T p1, T p2)
+            where T : Vector3
+            {
+                return new Vector3(p0.x + p1.x + p2.x / 3, p0.y + p1.y + p2.y / 3);
+            }
+            public static float TriangleArea(float a, float b, float c)
+            {
+                double s = (a + b + c) / 2.0f;
+                return (float)Math.Sqrt(s * (s - (double)a) * (s - (double)b) * (s - (double)c));
+            }
+            public static bool CheckIfValidTriangle<T>(T v1, T v2, T v3, out float a, out float b, out float c)
+            where T : Vector3
+            {
+                a = Vector2.Distance(new Vector2(v1.x, v1.y), new Vector2(v2.x, v2.y));
+                b = Vector2.Distance(new Vector2(v2.x, v2.y), new Vector2(v3.x, v3.y));
+                c = Vector2.Distance(new Vector2(v3.x, v3.y), new Vector2(v1.x, v1.y));
+
+                if (a + b <= c || a + c <= b || b + c <= a)
+                    return false;
+                return true;
+            }
+        }
+    
+    }
+}
---
+++ RenderingPipeline/VectorClass.cs
@@ -0,0 +1,193 @@
+﻿using System.ComponentModel;
+using System.Numerics;
+
+
+/*
+Lyras Attempt at Vector3 class, based on unities Vector3 class 
+ps: i hate vectors
+*/
+namespace RenderPipeline
+{
+     
+
+    public class Vector3
+    {
+        public float x; 
+        public float y;
+        public float z;
+        public void set(float x,float y,float z)
+        {
+            this.x = x; this.y = y; this.z = z;
+        }
+        
+        public Vector3(float x = 0f , float y = 0f, float z = 0f)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        //Returns a normalized vector based on the current vector. The normalized vector has a magnitude of 1 and is in the same direction as the current vector. Returns a zero vector If the current vector is too small to be normalized.
+
+        public Vector3 normalize() {
+            float nx = this.x; 
+            float ny= this.y;
+            float nz = this.z;
+            float mag = this.magnitude();
+            nx = nx / mag;
+            ny = ny / mag;
+            nz = nz / mag;
+            return new Vector3(nx, ny, nz);
+        }
+
+        //magnitude	Returns the length of this vector (Read Only).
+
+        public float magnitude() { 
+            return (float)Math.Sqrt((double)Math.Pow((double)this.x, 2) + (double)Math.Pow((double)this.y, 2) + (double)Math.Pow((double)this.z, 2));
+        }
+        //Returns the squared length of this vector (Read Only).
+        public float sqrMagnitude() {
+            return (float)Math.Pow((double)this.x, 2) + (float)Math.Pow((double)this.y, 2) + (float)Math.Pow((double)this.z, 2);
+        }
+
+        public (int,int,int) GetIntTupel()
+        {
+            return ((int)this.x, (int)this.y, (int)this.z);
+        }
+        /*
+        operator -	Subtracts one vector from another.
+        operator !=	Returns true if vectors are different.
+        operator *	Multiplies a vector by a number.
+        operator /	Divides a vector by a number.
+        operator +	Adds two three-dimensional vectors with component-wise addition.
+        operator ==	Returns true if two vectors are approximately equal.
+        */
+
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
+        }
+        public static Vector3 operator -(Vector3 a, Vector3 b) {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector3 operator *(Vector3 a, float b) {
+            return new Vector3(a.x*b,a.y*b,a.z*b);
+        }
+
+        public static bool operator ==(Vector3 a, Vector3 b) {
+            return (a.x == b.x && a.y == b.y && a.z == b.z);
+        }
+
+        public static bool operator !=(Vector3 a, Vector3 b) {
+            return (a.x != b.x && a.y != b.y && a.z != b.z);
+        }
+
+        public static Vector3 operator /(Vector3 a, float b) {
+            if (b == 0)
+            {
+                return a; // error msg would be useful i presume
+            }
+            return new Vector3(a.x / b, a.y / b, a.z / b);
+        }
+
+        //----------- Static Methods (Documentation overview from Unity) -------------------
+
+
+        //Angle	Calculates the angle between two vectors.
+
+        public static float Angle(Vector3 from, Vector3 to)
+        {
+            float denominator = (float)Math.Sqrt(from.sqrMagnitude() * to.sqrMagnitude());
+            if (denominator < 1e-15f)
+                return 0f;
+            float dot = Dot(from, to) / denominator;
+            dot = Math.Clamp(dot, -1f, 1f);
+            return (float)(Math.Acos(dot) * (180.0 / Math.PI));
+        }
+
+        //ClampMagnitude	Returns a copy of vector with its magnitude clamped to maxLength.
+        public static Vector3 ClampMagnitude(Vector3 vector, float maxLength)
+        {
+            float sqrMag = vector.sqrMagnitude();
+            if (sqrMag > maxLength * maxLength)
+            {
+                float mag = (float)Math.Sqrt(sqrMag);
+                return vector * (maxLength / mag);
+            }
+            return vector;
+        }
+
+        //Cross	Calculates the cross product of two three-dimensional vectors.
+
+        public static Vector3 Cross(Vector3 lhs, Vector3 rhs)
+        {
+            return new Vector3(
+                lhs.y * rhs.z - lhs.z * rhs.y,
+                lhs.z * rhs.x - lhs.x * rhs.z,
+                lhs.x * rhs.y - lhs.y * rhs.x
+            );
+        }
+        //Distance	Calculates the distance between two three-dimensional points.
+
+        public static float Distance(Vector3 a, Vector3 b)
+        {
+            return (a - b).magnitude();
+        }
+        //Dot	Calculates the dot product of two three-dimensional vectors defined in the same coordinate space.
+
+        public static float Dot(Vector3 lhs, Vector3 rhs) //left hand side - right hand side
+        {
+            return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
+        }
+
+
+        //Lerp	Linearly interpolates between two points.
+
+        //LerpUnclamped	Linearly interpolates between two vectors.
+
+        //Max	Returns a vector that is made from the largest components of two vectors.
+
+        //Min	Returns a vector that is made from the smallest components of two vectors.
+
+        //MoveTowards	Calculate a position between the points specified by current and target, moving no farther than the distance specified by maxDistanceDelta.
+
+        //Normalize	Makes this vector have a magnitude of 1.
+
+        //OrthoNormalize	Makes vectors normalized and orthogonal to each other.
+
+        //Project	Projects a vector onto another vector.
+
+        //ProjectOnPlane	Projects a vector onto a plane.
+        public static Vector3 ProjectOnPlane(Vector3 vector, Vector3 planeNormal)
+        {
+            float sqrMag = Dot(planeNormal, planeNormal);
+            if (sqrMag < 1e-15f) //magic very small number
+                return vector;
+            // Projection of vector onto the normal
+            Vector3 projection = planeNormal * (Dot(vector, planeNormal) / sqrMag);
+            // Subtract it -> projection on plane
+            return vector - projection;
+        }
+
+        //Reflect	Reflects a vector off the plane defined by a normal.
+
+        //RotateTowards	Rotates a vector current towards target.
+
+        //Scale	Multiplies two vectors component-wise.
+
+        //SignedAngle	Calculates the signed angle between vectors from and to in relation to axis.
+
+        //Slerp	Spherically interpolates between two three-dimensional vectors.
+
+        //SlerpUnclamped	Spherically interpolates between two vectors.
+
+        //SmoothDamp	Gradually changes a vector towards a desired goal over time.
+
+
+
+
+    }
+
+
+}
